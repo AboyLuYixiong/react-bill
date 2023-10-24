df73bf28eb66de4b01c0037f1536890960da2a7b
AboyLuYixiong <2084896183@qq.com>
2023-10-24 09:21:46 +0800

    第一次项目提交

---
+++ src/pages/Layout/index.js
@@ -0,0 +1,52 @@
+import { TabBar } from "antd-mobile"
+import { useEffect } from "react"
+import { Outlet } from "react-router-dom"
+import { useDispatch } from 'react-redux'
+import { getBillList } from "@/store/modules/billStore"
+import './index.scss'
+import {
+  BillOutline,
+  CalculatorOutline,
+  AddCircleOutline
+} from 'antd-mobile-icons'
+
+const tabs = [
+  {
+    key: '/month',
+    title: '月度账单',
+    icon: <BillOutline />,
+  },
+  {
+    key: '/new',
+    title: '记账',
+    icon: <AddCircleOutline />,
+  },
+  {
+    key: '/year',
+    title: '年度账单',
+    icon: <CalculatorOutline />,
+  }
+]
+
+const Layout = () => {
+  const dispatch = useDispatch()
+  useEffect(() => {
+    dispatch(getBillList())
+  }, [dispatch])
+  return (
+    <div className="layout">
+      <div className="container">
+        <Outlet />
+      </div>
+      <div className="footer">
+        <TabBar>
+          {tabs.map(item => (
+            <TabBar.Item key={item.key} icon={item.icon} title={item.title} />
+          ))}
+        </TabBar>
+      </div>
+    </div>
+  )
+}
+
+export default Layout
---
+++ src/pages/Month/index.js
@@ -0,0 +1,61 @@
+import { NavBar, DatePicker } from 'antd-mobile'
+import { useState } from 'react'
+import classNames from 'classnames'
+import './index.scss'
+
+const Month = () => {
+  // 控制弹框的打开和关闭
+  const [dataVisible, setDataVisible] = useState(false)
+
+  const onConfirm = () => {
+    setDataVisible(false)
+  }
+
+  return (
+    <div className="monthlyBill">
+      <NavBar className="nav" backArrow={false}>
+        月度收支
+      </NavBar>
+      <div className="content">
+        <div className="header">
+          {/* 时间切换区域 */}
+          <div className="date" onClick={() => setDataVisible(true)}>
+            <span className="text">
+              2023 | 3月账单
+            </span>
+            {/* 思路：根据当前弹框打开的状态控制expand类名是否存在 */}
+            <span className={classNames('arrow', dataVisible && 'expand')}></span>
+          </div>
+          {/* 统计区域 */}
+          <div className='twoLineOverview'>
+            <div className="item">
+              <span className="money">{100}</span>
+              <span className="type">支出</span>
+            </div>
+            <div className="item">
+              <span className="money">{200}</span>
+              <span className="type">收入</span>
+            </div>
+            <div className="item">
+              <span className="money">{200}</span>
+              <span className="type">结余</span>
+            </div>
+          </div>
+          {/* 时间选择器 */}
+          <DatePicker
+            className="kaDate"
+            title="记账日期"
+            precision="month"
+            visible={dataVisible}
+            onCancel={() => setDataVisible(false)}
+            onConfirm={onConfirm}
+            onClose={() => setDataVisible(false)}
+            max={new Date()}
+          />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Month
