2eab1756adce447bea20e547e341e8051386264b
AboyLuYixiong <2084896183@qq.com>
2023-10-24 10:47:12 +0800

    月度账单-初始化统计数据渲染

--- src/pages/Month/index.js
+++ src/pages/Month/index.js
@@ -1,5 +1,5 @@
 import { NavBar, DatePicker } from 'antd-mobile'
-import { useMemo, useState } from 'react'
+import { useEffect, useMemo, useState } from 'react'
 import classNames from 'classnames'
 import dayjs from 'dayjs'
 import { useSelector } from 'react-redux'
@@ -37,6 +37,15 @@ const Month = () => {
       total: pay + income
     }
   }, [currentMonthList])
+
+  // 初始化的时候把当前月的统计数据显示出来
+  useEffect(() => {
+    const nowDate = dayjs().format('YYYY-MM')
+    // 边界值控制
+    if (monthGroup[nowDate]) {
+      setMonthList(monthGroup[nowDate])
+    }
+  }, [monthGroup])
 
   // 确认回调
   const onConfirm = (date) => {
